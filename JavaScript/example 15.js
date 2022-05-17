b952c7ef6631f0d0e1f8330be70615a2bf2ffb4e
Ana-Gochashvili <gochashvili.ana@gmail.com>
2022-05-17 20:54:58 +0400

    add new JS files

---
+++ JavaScript/example 15.js
@@ -0,0 +1,12 @@
+let test = "some text??";
+
+if(typeof test === 'string'){
+    if(test.includes('?')) {
+        console.log(true);
+    }else {
+        console.log(false);
+    }
+}else{
+    throw "It is not a string";
+}
+
---
+++ JavaScript/example 8.js
@@ -0,0 +1,14 @@
+let x = 358;
+let y = 847;
+
+maxis(x,y);
+
+
+function maxis(x,y){
+    if(x > y){
+        console.log(x);
+    }else{
+        console.log(y);
+    }
+
+}
