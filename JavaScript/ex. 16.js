6af1f8785327d45eb18a0ca6f0ed68eb5e8875e5
Ana-Gochashvili <gochashvili.ana@gmail.com>
2022-05-24 00:49:47 +0400

    add 2 new jsfiles

--- JavaScript/ex. 16.js
+++ JavaScript/ex. 16.js
@@ -1,9 +1,6 @@
-const text = 'JavaScript is the programming language of HTML and the Web. ' +
+const jsText = 'JavaScript is the programming language of HTML and the Web. ' +
     'JavaScript is easy to learn. This tutorial will teach you JavaScript from basic to advanced.'
-unWords(text);
 
-function unWords(text) {
-    for (let w of text) {
-        console.log(w.text + ': ' + w.length)
-    }
-}
+let arrOne = jsText.split(' ');
+
+arrOne.forEach((word) => console.log(word + ':' + word.length));
---
+++ JavaScript/ex. 21.js
@@ -0,0 +1,5 @@
+setTimeout(myAlertF, 5000);
+
+function myAlertF() {
+    console.log(alert("Hello World!"));
+}
