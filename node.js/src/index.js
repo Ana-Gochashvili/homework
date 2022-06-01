c7628f0d1a9c57b2bc71831dee24e600761c811a
Ana-Gochashvili <gochashvili.ana@gmail.com>
2022-06-01 23:31:17 +0400

    added new node.js files.

--- node.js/src/index.js
+++ node.js/src/index.js
@@ -1,34 +1,21 @@
 class Point {
     #x;
     #y;
-
     constructor(x, y) {
         this.#x = x;
         this.#y = y;
     }
-
     get x() {
         return this.#x;
     }
-
     get y() {
         return this.#y;
     }
-
-    set x(x2) {
-        this.#x = x2;
+    distance(pnt) {
+        return Math.sqrt(((pnt.x- this.#x) ** 2) + ((pnt.y - this.#y) ** 2))
     }
-
-    set y(y2) {
-        this.#y = y2;
-    }
-
-    distance() {
-        return Math.sqrt(((x2 - this.#x) ** 2) + ((y2 - this.#y) ** 2))
-    }
-
     toString() {
-        return Point.toString();
+        return `Point(${this.#x},${this.#y})`;
     }
 }
 
@@ -36,15 +23,9 @@ let point1 = new Point(3, -4);
 
 let point2 = new Point(6, 0);
 
-
-console.log(point1.toString);
-
 console.log(point2);
 console.log(point1);
 
-// function distance(point1,point2){
-//
-//     return Math.sqrt(((point2.x-point1.x)**2)+((point2.y-point1.y)**2))
-// }
+console.log(point2.distance(point1));
 
-console.log(point2.distance);
+console.log(toString(point1));
